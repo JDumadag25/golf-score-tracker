608714e165623d1cf0438935c633a4597f000522
Justin Dumadag <justin.dumadag@gmail.com>
2024-04-24 17:29:22 -0400

    Start screen stylying wip

--- src/screens/StartScreen.js
+++ src/screens/StartScreen.js
@@ -1,70 +1,73 @@
 import React, { useContext } from 'react';
-import {
-    View,
-    Text,
-    TextInput,
-    TouchableOpacity,
-    StyleSheet,
-} from 'react-native';
+import { View, Text, TextInput, TouchableOpacity } from 'react-native';
 import ScoreContext from '../context/ScoreContext';
 
 const StartScreen = ({ navigation }) => {
-    const { submitRoundType } = useContext(ScoreContext);
+    const { submitRoundType, roundType } = useContext(ScoreContext);
 
     const setRoundType = (type) => {
         submitRoundType(type);
     };
+
     return (
-        <View>
-            <View>
-                <Text>Please select your round type</Text>
+        <View className="flex-1 items-center ">
+            <View className="flex-initial mt-10">
+                <Text className="text-3xl">Please select your round type</Text>
             </View>
 
-            <View>
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => setRoundType(18)}
-                >
-                    <Text>18 Holes</Text>
-                </TouchableOpacity>
-
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => setRoundType(3)}
-                >
-                    <Text>9 Holes</Text>
-                </TouchableOpacity>
+            <View className="flex-initial mt-5 h-20 flex-row p-5 ">
+                <View className=" flex-auto mr-5">
+                    <TouchableOpacity
+                        className={
+                            roundType === 18
+                                ? `bg-green-300 text-white font-bold py-2 px-4 rounded-full`
+                                : `bg-gray-300 text-white font-bold py-2 px-4 rounded-full`
+                        }
+                        onPress={() => setRoundType(18)}
+                    >
+                        <Text className="text-center">18 Holes</Text>
+                    </TouchableOpacity>
+                </View>
+                <View className="flex-auto ">
+                    <TouchableOpacity
+                        className={
+                            roundType === 9
+                                ? `bg-green-300 text-white font-bold py-2 px-4 rounded-full`
+                                : `bg-gray-300 text-white font-bold py-2 px-4 rounded-full`
+                        }
+                        onPress={() => setRoundType(9)}
+                    >
+                        <Text className="text-center">9 Holes</Text>
+                    </TouchableOpacity>
+                </View>
             </View>
 
-            <View style={styles.inputStyle}>
-                <TextInput placeholder="Par" />
-            </View>
+            {/* <View className="flex-initial w-full items-center justify-center ">
+                <TextInput
+                    placeholder="Enter Course Par"
+                    className="`w-full bg-neutral-100 dark:bg-neutral-900 border border-black/20 dark:border-white/20 rounded-md h-12 px-4 text-neutral-950 dark:text-neutral-50"
+                />
+            </View> */}
 
-            <View>
-                <TouchableOpacity
-                    style={styles.button}
-                    onPress={() => navigation.navigate('Score Screen')}
-                >
-                    <Text>Play</Text>
-                </TouchableOpacity>
+            <View className="h-14 w-32 mt-5  items-center justify-center ">
+                {roundType ? (
+                    <TouchableOpacity
+                        className="bg-green-300 active:bg-red-700 text-white font-bold py-2 px-4 rounded-full h-full w-full items-center justify-center disabled"
+                        onPress={() => navigation.navigate('Score Screen')}
+                    >
+                        <Text className="text-center text-lg">Play</Text>
+                    </TouchableOpacity>
+                ) : (
+                    <TouchableOpacity
+                        className="bg-gray-300 active:bg-red-700 text-white font-bold py-2 px-4 rounded-full h-full w-full items-center justify-center disabled"
+                        disabled={true}
+                    >
+                        <Text className="text-center text-lg">Play</Text>
+                    </TouchableOpacity>
+                )}
             </View>
         </View>
     );
 };
-
-const styles = StyleSheet.create({
-    button: {
-        height: 40,
-        width: 80,
-        borderWidth: 2,
-        borderColor: 'red',
-    },
-
-    inputStyle: {
-        borderColor: 'black',
-        borderWidth: 1,
-        height: 30,
-    },
-});
 
 export default StartScreen;
