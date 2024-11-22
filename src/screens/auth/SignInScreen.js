df96cdd68db584e264bc3e8a33ff2fd0c6fc1830
Justin Dumadag <justin.dumadag@gmail.com>
2024-11-21 19:36:47 -0500

    sign in and up screen

--- src/screens/auth/SignInScreen.js
+++ src/screens/auth/SignInScreen.js
@@ -1,32 +1,43 @@
-import React, { useContext, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React from 'react';
+import { View, Text, TouchableOpacity, TextInput } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
 
 const SignInScreen = ({ navigation }) => {
+    const insets = useSafeAreaInsets();
+
     return (
-        <View className="flex-1 justify-center items-center ">
-            <Text>Golf Score Tracker</Text>
-            <Text>Username</Text>
-            <TextInput className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-52 p-2.5" />
-            <Text>Password</Text>
-            <TextInput className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-52 p-2.5" />
-            <TouchableOpacity
-                className="bg-green-300 text-white font-bold py-2 px-4 rounded-full"
-                onPress={() => navigation.navigate('Start Screen')}
-            >
-                <Text>Log In</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                className="bg-green-300 text-white font-bold py-2 px-4 rounded-full"
-                onPress={() => navigation.navigate('Sign Up Screen')}
-            >
-                <Text>Sign Up</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-                className="bg-green-300 text-white font-bold py-2 px-4 rounded-full"
-                onPress={() => navigation.navigate('Main')}
-            >
-                <Text>TEMP GO TO SCORES</Text>
-            </TouchableOpacity>
+        <View
+            className="flex flex-col top-5"
+            style={{
+                paddingTop: insets.top,
+                paddingBottom: insets.bottom,
+                paddingLeft: insets.left,
+                paddingRight: insets.right,
+            }}
+        >
+            <View className="basis-10 justify-center items-center">
+                <Text>Sign In</Text>
+            </View>
+            <View className="basis-1/2 justify-center items-center ">
+                <View className="basis-1/4 w-full px-6 ">
+                    <Text>Username</Text>
+                    <TextInput className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 mt-3" />
+                </View>
+                <View className="basis-1/4 w-full px-6 ">
+                    <Text>Password</Text>
+                    <TextInput className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block p-2.5 mt-3" />
+                </View>
+            </View>
+            <View className="basis-1/2  ">
+                <View className="mx-8 ">
+                    <TouchableOpacity
+                        onPress={() => navigation.navigate('Sign In Screen')}
+                        className="items-center justify-center h-11 bg-blue-700 hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-5 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        <Text className="text-white">Sign In</Text>
+                    </TouchableOpacity>
+                </View>
+            </View>
         </View>
     );
 };
